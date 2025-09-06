7c58c73ad56731a4b3cf2ecccc4ccbbeed7e8024
NetHaven <NetHaven@gmail.com>
2025-09-06 01:20:18 -0400

    Created MenuItemEvent class for event constants for MenuItem component

---
+++ src/events/menu/MenuItemEvent.js
@@ -0,0 +1,12 @@
+import Event from "../Event.js";
+
+/**
+ * Event constants for MenuItem component
+ */
+export default class MenuItemEvent extends Event {
+    static SELECT = "select";
+
+    constructor() {
+        super();
+    }
+}
