02d569d521c1b269b26bb4d2f5f83192a384aa79
NetHaven <NetHaven@gmail.com>
2025-09-06 04:00:05 -0400

    Created CheckboxMenuItemEvent constant class for CheckboxMenuItem component

---
+++ src/events/menu/CheckboxMenuItemEvent.js
@@ -0,0 +1,12 @@
+import Event from "../Event.js";
+
+/**
+ * Event constants for CheckboxMenuItem component
+ */
+export default class CheckboxMenuItemEvent extends Event {
+    static CHANGE = "change";
+
+    constructor() {
+        super();
+    }
+}
