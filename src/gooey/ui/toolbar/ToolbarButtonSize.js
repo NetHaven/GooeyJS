3bd7f454eb880edd2db0f89a22dc8a0b031111dc
NetHaven <NetHaven@gmail.com>
2025-09-08 20:17:01 -0400

    Added static constant class for toolbar button sizes

---
+++ src/gooey/ui/toolbar/ToolbarButtonSize.js
@@ -0,0 +1,7 @@
+export default class ToolbarButtonSize {
+    static TINY = 'tiny';
+    static SMALL = 'small';
+    static MEDIUM = 'medium';
+    static LARGE = 'large';
+    static XLARGE = 'xlarge';
+}
