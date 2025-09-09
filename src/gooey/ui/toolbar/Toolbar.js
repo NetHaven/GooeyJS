11c303125bea02013248a083ba94759994e3ae58
NetHaven <NetHaven@gmail.com>
2025-09-08 21:08:17 -0400

    Created Toolbar component class

---
+++ src/gooey/ui/toolbar/Toolbar.js
@@ -0,0 +1,41 @@
+import Container from '../Container.js';
+import ToolbarButtonSize from './ToolbarButtonSize.js';
+
+export default class Toolbar extends Container {
+    constructor () {
+        super();
+        
+        // Check for wrap attribute during initialization
+        if (this.hasAttribute("wrap")) {
+            this.wrap = true;
+        }
+    }
+
+    get buttonSize() {
+        return this.getAttribute("buttonsize");
+    }
+
+    set buttonSize(val) {
+        switch (val) {
+            case ToolbarButtonSize.TINY:
+            case ToolbarButtonSize.SMALL:
+            case ToolbarButtonSize.MEDIUM:
+            case ToolbarButtonSize.LARGE: 
+            case ToolbarButtonSize.XLARGE: this.setAttribute("buttonsize", val);
+        }
+    }
+
+    get wrap() {
+        return this.hasAttribute("wrap");
+    }
+
+    set wrap(val) {
+        if (val) {
+            this.setAttribute("wrap", "");
+            this.style.flexWrap = "wrap";
+        } else {
+            this.removeAttribute("wrap");
+            this.style.flexWrap = "nowrap";
+        }
+    }
+}
