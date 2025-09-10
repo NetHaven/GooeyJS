f656748456aab6d9a4e0842c706f033e8d36c8e3
NetHaven <NetHaven@gmail.com>
2025-09-09 22:11:31 -0400

    Created ToggleButtonEvent class

---
+++ src/gooey/events/button/ToggleButtonEvent.js
@@ -0,0 +1,12 @@
+import Event from "../Event.js";
+
+/**
+ * Event constants for ToggleButton component
+ */
+export default class ToggleButtonEvent extends Event {
+    static TOGGLE = "toggle";
+
+    constructor() {
+        super();
+    }
+}
