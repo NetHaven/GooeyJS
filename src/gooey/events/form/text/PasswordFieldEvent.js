9099511983a482e902bd81faed0ce23265361028
NetHaven <NetHaven@gmail.com>
2025-09-29 18:52:48 -0400

    Class for PasswordField event constants

---
+++ src/gooey/events/form/text/PasswordFieldEvent.js
@@ -0,0 +1,11 @@
+import TextFieldEvent from './TextFieldEvent.js';
+
+/**
+ * Event constants for PasswordField component
+ * Extends TextFieldEvent to inherit common text field events
+ */
+export default class PasswordFieldEvent extends TextFieldEvent {
+    constructor() {
+        super();
+    }
+}
