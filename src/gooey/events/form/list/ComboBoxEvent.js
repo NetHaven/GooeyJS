94ac22f945a7649adf53029b32286cdea9213fe7
NetHaven <NetHaven@gmail.com>
2025-10-03 04:25:21 -0400

    Class for ComboBox event constants

---
+++ src/gooey/events/form/list/ComboBoxEvent.js
@@ -0,0 +1,13 @@
+import Event from "../../Event.js";
+
+/**
+ * Event constants for ComboBox component
+ */
+export default class ComboBoxEvent extends Event {
+    static DROPDOWN_OPEN = "dropdownOpen";
+    static DROPDOWN_CLOSE = "dropdownClose";
+
+    constructor() {
+        super();
+    }
+}
