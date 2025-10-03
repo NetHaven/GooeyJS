1ffadb4c9cdaaa004af4e7909844a7dfb6c8b522
NetHaven <NetHaven@gmail.com>
2025-10-03 04:24:28 -0400

    Class for DropDownList event constants

---
+++ src/gooey/events/form/list/DropDownListEvent.js
@@ -0,0 +1,12 @@
+import Event from "../../Event.js";
+
+/**
+ * Event constants for DropDownList component
+ */
+export default class DropDownListEvent extends Event {
+    static CHANGE = "change";
+
+    constructor() {
+        super();
+    }
+}
