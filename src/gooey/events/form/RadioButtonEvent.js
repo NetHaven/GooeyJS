cd00a6afca72efd420e0b979c7cd87be038afb0c
NetHaven <NetHaven@gmail.com>
2025-10-06 07:23:55 -0400

    Class for RadioButton event constants

---
+++ src/gooey/events/form/RadioButtonEvent.js
@@ -0,0 +1,12 @@
+import Event from "../Event.js";
+
+/**
+ * Event constants for RadioButton component
+ */
+export default class RadioButtonEvent extends Event {
+    static CHANGE = "change";
+
+    constructor() {
+        super();
+    }
+}
