d4826c7cde7d6cb95f6662a159f642e1ebe8f78b
NetHaven <NetHaven@gmail.com>
2025-10-10 06:52:17 -0400

    Class for Border style event constants

---
+++ src/gooey/ui/BorderStyle.js
@@ -0,0 +1,12 @@
+export default class BorderStyle {
+    static DASHED = "DASHED";
+    static DOTTED = "DOTTED";
+    static DOUBLE = "DOUBLE";
+    static GROOVE = "GROOVE";
+    static HIDDEN = "HIDDEN";
+    static INSET = "INSET";
+    static NONE = "NONE";
+    static OUTSET = "OUTSET";
+    static RIDGE = "RIDGE";
+    static SOLID = "SOLID";
+}
