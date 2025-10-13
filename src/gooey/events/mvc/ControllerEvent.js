ceeb44b9d0c1e190763f72e595db1d7d78ca1312
NetHaven <NetHaven@gmail.com>
2025-10-13 05:15:42 -0400

    Class for Controller event constants

---
+++ src/gooey/events/mvc/ControllerEvent.js
@@ -0,0 +1,14 @@
+import Event from "../Event";
+
+export default class ControllerEvent extends Event {
+    static INITIALIZED = "initialized";
+    static VIEW_ATTACHED = "viewAttached";
+    static MODEL_BOUND = "modelBound";
+    static BEFORE_ACTION = "beforeAction";
+    static AFTER_ACTION = "afterAction";
+    static ERROR = "error";
+
+    constructor() {
+        super();
+    }
+}
