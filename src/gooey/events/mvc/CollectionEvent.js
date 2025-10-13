90515f7d1c83de44512774cdd9cc52dfb830eb80
NetHaven <NetHaven@gmail.com>
2025-10-13 04:49:47 -0400

    Class for Collection event constants

---
+++ src/gooey/events/mvc/CollectionEvent.js
@@ -0,0 +1,15 @@
+import Event from "./Event.js";
+
+export default class CollectionEvent extends Event {
+    static ADD = "add";
+    static REMOVE = "remove";
+    static RESET = "reset";
+    static SORT = "sort";
+    static UPDATE = "update";
+    static SYNC = "sync";
+    static ERROR = "error";
+
+    constructor() {
+        super();
+    }
+}
