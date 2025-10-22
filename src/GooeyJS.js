e8734ca1e40d5daa62175fce71aad0d0e0f78e96
NetHaven <NetHaven@gmail.com>
2025-10-21 20:51:35 -0400

    Integrate RichTextEditor class with RichTextEditor template

--- src/GooeyJS.js
+++ src/GooeyJS.js
@@ -139,6 +139,7 @@ export default class GooeyJS {
             Template.load(`${templatePath}/ProgressBar.html`, "ui-ProgressBar"),
             Template.load(`${templatePath}/RadioButton.html`, "ui-RadioButton"),
             Template.load(`${templatePath}/RadioButtonGroup.html`, "ui-RadioButtonGroup"),
+            Template.load(`${templatePath}/RichTextEditor.html`, "ui-RichTextEditor"),
             Template.load(`${templatePath}/Spinner.html`, "ui-Spinner"),
             Template.load(`${templatePath}/SplitPanel.html`, "ui-SplitPanel"),
             Template.load(`${templatePath}/Tab.html`, "ui-Tab"),
