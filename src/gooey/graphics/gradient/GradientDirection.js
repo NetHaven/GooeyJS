b3fabe1f4233b1a12165feba9e2f390d5810cae1
NetHaven <NetHaven@gmail.com>
2025-10-22 02:37:22 -0400

    Class for Gradient direction constants

---
+++ src/gooey/graphics/gradient/GradientDirection.js
@@ -0,0 +1,10 @@
+export default class GradientDirection {
+    static BOTTOM = "to bottom";
+    static LEFT = "to left";
+    static LEFT_BOTTOM = "to left bottom";
+    static LEFT_TOP = "to left top";
+    static RIGHT = "to right";
+    static RIGHT_BOTTOM = "to right bottom";
+    static RIGHT_TOP = "to right top";
+    static TOP = "to top";
+}
