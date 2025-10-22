b0675efde9b58106bda7f4f720e0ceed42889e30
NetHaven <NetHaven@gmail.com>
2025-10-22 02:43:53 -0400

    Class for Key constants

---
+++ src/gooey/io/Key.js
@@ -0,0 +1,8 @@
+export default class Key {
+    static ARROW_DOWN = "ArrowDown";
+    static ARROW_LEFT = "ArrowLeft";
+    static ARROW_RIGHT = "ArrowRight";
+    static ARROW_UP = "ArrowUp";
+    static ENTER = "Enter";
+    static ESCAPE = "Escape";
+}
