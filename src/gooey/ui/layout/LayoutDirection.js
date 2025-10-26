48f45958117335efd635fe59e1370bddccae7a87
NetHaven <NetHaven@gmail.com>
2025-10-26 11:56:05 -0400

    Class for layout direction constants

---
+++ src/gooey/ui/layout/LayoutDirection.js
@@ -0,0 +1,6 @@
+ export default class LayoutDirection {
+    static COLUMN = "column";
+    static COLUMN_REVERSE = "column-reverse";
+    static ROW = "row";
+    static ROW_REVERSE = "row-reverse";
+ }
