217ec0c1f97e9c8dcdf53c51c5f7a7c3230689d5
NetHaven <NetHaven@gmail.com>
2025-10-26 13:24:14 -0400

    Class for layout justification constants

---
+++ src/gooey/ui/layout/LayoutJustify.js
@@ -0,0 +1,8 @@
+export default class LayoutJustify {
+    static CENTER = "center";
+    static FLEX_END = "flex-end";
+    static FLEX_START = "flex-start";
+    static SPACE_AROUND = 'space-around';
+    static SPACE_BETWEEN = "space-between";
+    static SPACE_EVENLY = "space-evenly";
+}
