6390da18a35c89f84b0a409591c2b6890d1609d1
NetHaven <NetHaven@gmail.com>
2025-10-26 13:27:43 -0400

    Class for layout alignment constants

---
+++ src/gooey/ui/layout/LayoutAlign.js
@@ -0,0 +1,7 @@
+export default class LayoutAlign {
+    static BASELINE = "baseline";
+    static CENTER = "center";
+    static FLEX_END = "flex-end";
+    static FLEX_START = "flex-start";
+    static STRETCH = "stretch";
+}
