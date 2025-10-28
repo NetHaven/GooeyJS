016f2e399b880e57f01847072346c209737d8f4b
NetHaven <NetHaven@gmail.com>
2025-10-27 23:48:42 -0400

    Added setters, getters and default layout for Layout class

--- src/gooey/ui/layout/Layout.js
+++ src/gooey/ui/layout/Layout.js
@@ -1,8 +1,82 @@
+import LayoutAlign from './LayoutAlign.js';
+import LayoutDirection from './LayoutDirection.js';
+import LayoutJustify from './LayoutJustify.js';
+import LayoutType from './LayoutType.js';
+import LayoutWrap from './LayoutWrap.js';
+
 export default class Layout {
-    static BORDER = "BORDER";
-    static CARD = "CARD";
-    static GRID = "GRID";
-    static HBOX = "HBOX";
-    static FLOW = "FLOW";
-    static VBOX = "VBOX";        
+    constructor() {
+        if (!this.type) {
+            this.type = LayoutType.FLOW;
+        }
+    }
+
+    get align() {
+        return this.getAttribute("align");
+    }
+
+    get direction() {
+        return this.getAttribute("direction");
+    }
+
+    get justify() {
+        return this.getAttribute("justify");
+    }
+
+    get type() {
+        return this.getAttribute("type");
+    }
+
+    get wrap() {
+        return this.getAttribute("wrap");
+    }
+
+    set align(val) {
+        switch (val)  {
+            case LayoutAlign.BASELINE: 
+            case LayoutAlign.CENTER:
+            case LayoutAlign.FLEX_END:
+            case LayoutAlign.FLEX_START: 
+            case LayoutAlign.STRETCH:       this.setAttribute("align", val);
+        }
+    }
+
+    set direction(val) {
+        switch (val)  {
+            case LayoutDirection.COLUMN:
+            case LayoutDirection.COLUMN_REVERSE:
+            case LayoutDirection.ROW:
+            case LayoutDirection.ROW_REVERSE: this.setAttribute("direction", val);
+        }
+    }
+
+    set justify(val) {
+        switch (val)  {
+            case LayoutJustify.CENTER: 
+            case LayoutJustify.FLEX_END:
+            case LayoutJustify.FLEX_START:
+            case LayoutJustify.SPACE_AROUND:
+            case LayoutJustify.SPACE_BETWEEN:
+            case LayoutJustify.SPACE_EVENLY: this.setAttribute("justify", val);
+        }
+    }
+
+    set type(val) {
+        switch (val)  {
+            case LayoutType.BORDER:
+            case LayoutType.CARD:
+            case LayoutType.FLOW:
+            case LayoutType.GRID:
+            case LayoutType.HBOX:
+            case LayoutType.VBOX:   this.setAttribute("type", val);    
+        }
+    }
+
+    set wrap(val) {
+        switch (val)  {
+            case LayoutWrap.NO_WRAP: 
+            case LayoutWrap.WRAP:
+            case LayoutWrap.WRAP_REVERSE: this.setAttribute("wrap", val);
+        }
+    }
 }
