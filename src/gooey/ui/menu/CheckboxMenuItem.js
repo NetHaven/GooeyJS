bafba3f8aba26d58500c49dabe25e4f207c7ea08
NetHaven <NetHaven@gmail.com>
2025-11-02 17:39:44 -0500

    Implement CheckboxMenuItem.observedAttributes static getter

--- src/gooey/ui/menu/CheckboxMenuItem.js
+++ src/gooey/ui/menu/CheckboxMenuItem.js
@@ -4,6 +4,10 @@ import MouseEvent from '../../events/MouseEvent.js';
 import Template from '../../util/Template.js';
 
 export default class CheckboxMenuItem extends MenuItem {
+    static get observedAttributes() {
+        return [...super.observedAttributes, 'checked'];
+    }
+
     constructor () {
         super();
 
