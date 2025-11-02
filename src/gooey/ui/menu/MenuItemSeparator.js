89df1f30b45e07bba887b8ca958c6385d9ecfb3d
NetHaven <NetHaven@gmail.com>
2025-11-02 17:06:28 -0500

    Implement MenuItemSeparator.observedAttributes static getter

--- src/gooey/ui/menu/MenuItemSeparator.js
+++ src/gooey/ui/menu/MenuItemSeparator.js
@@ -2,6 +2,10 @@ import Component from '../Component.js';
 import Template from '../../util/Template.js';
 
 export default class MenuItemSeparator extends Component {
+    static get observedAttributes() {
+        return [...super.observedAttributes];
+    }
+
     constructor() {
         super();
        
