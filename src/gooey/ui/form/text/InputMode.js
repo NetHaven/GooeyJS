43dcb9b72adbc3ab26291584dd67e23ce8985bc9
NetHaven <NetHaven@gmail.com>
2025-11-13 07:52:57 -0500

    Class containing static constants for text input modes so mobile browsers can provide the correct virtual keyboard

---
+++ src/gooey/ui/form/text/InputMode.js
@@ -0,0 +1,10 @@
+export default class InputMode {
+    static DECIMAL = "decimal";
+    static EMAIL = "email";
+    static NONE = "none";
+    static NUMERIC = "numeric";
+    static SEARCH = "search";
+    static TEL = "tel";
+    static TEXT = "text";
+    static URL = "url";
+}
