bfa5c071ca8a06cf493f0feef5819288d3b26e5b
NetHaven <NetHaven@gmail.com>
2025-11-16 17:13:24 -0500

    Added class attribute support

--- src/gooey/GooeyElement.js
+++ src/gooey/GooeyElement.js
@@ -2,9 +2,26 @@ import Observable from '../events/Observable.js';
 
 export default class GooeyElement extends Observable {
     static get observedAttributes() {
-        return ['id'];
+        return ['class', 'id'];
     }
+
     constructor () {
         super();
+    }
+
+    get class() {
+        return this.getAttribute("class");
+    }
+
+    get id() {
+        return this.getAttribute("id");
+    }
+
+    set class(val) {
+        this.setAttribute("class", val);
+    }
+
+    set id(val) {
+        this.setAttribute("id", val);
     }
 }
