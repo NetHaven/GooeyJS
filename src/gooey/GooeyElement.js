79f78e95707f8afc15fcfe6296b1434d004975ab
NetHaven <NetHaven@gmail.com>
2025-11-16 17:05:36 -0500

    Base class for all GooeyElements-not just UI components

---
+++ src/gooey/GooeyElement.js
@@ -0,0 +1,10 @@
+import Observable from '../events/Observable.js';
+
+export default class GooeyElement extends Observable {
+    static get observedAttributes() {
+        return ['id'];
+    }
+    constructor () {
+        super();
+    }
+}
