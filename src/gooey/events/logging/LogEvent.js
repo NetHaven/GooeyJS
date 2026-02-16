1c1c35db9d2b2d951932543bc76bf1ef5b874f12
NetHaven <NetHaven@gmail.com>
2026-02-16 17:48:41 -0500

    feat(01-01): create LogEvent constants class
    - Extends Event base class following DataStoreEvent pattern
    
    - Static constants: RECORD, LEVEL_CHANGE, HANDLER_ERROR, FLUSH
    
    - All values use log- prefixed kebab-case strings

---
+++ src/gooey/events/logging/LogEvent.js
@@ -0,0 +1,27 @@
+import Event from "../Event.js";
+
+/**
+ * Event constants for the GooeyJS logging system.
+ * Used with Observable's addValidEvent() and fireEvent() pattern.
+ *
+ * All event names use a "log-" prefix in kebab-case to avoid collision
+ * with other Observable event types.
+ */
+export default class LogEvent extends Event {
+
+    /** Fired when a log record is created and dispatched */
+    static RECORD = "log-record";
+
+    /** Fired when a logger's level is changed at runtime */
+    static LEVEL_CHANGE = "log-level-change";
+
+    /** Fired when a handler encounters an error during emit */
+    static HANDLER_ERROR = "log-handler-error";
+
+    /** Fired when a flush operation is requested */
+    static FLUSH = "log-flush";
+
+    constructor() {
+        super();
+    }
+}
