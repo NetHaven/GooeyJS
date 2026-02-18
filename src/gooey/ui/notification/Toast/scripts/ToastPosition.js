040ad377768f484903654759ac64253d64642fc9
NetHaven <NetHaven@gmail.com>
2026-02-18 03:18:45 -0500

    feat(11-01): create ToastType and ToastPosition attribute constants

---
+++ src/gooey/ui/notification/Toast/scripts/ToastPosition.js
@@ -0,0 +1,11 @@
+/**
+ * Toast position constants for Toast component
+ */
+export default class ToastPosition {
+    static TOP_LEFT = "top-left";
+    static TOP_CENTER = "top-center";
+    static TOP_RIGHT = "top-right";
+    static BOTTOM_LEFT = "bottom-left";
+    static BOTTOM_CENTER = "bottom-center";
+    static BOTTOM_RIGHT = "bottom-right";
+}
---
+++ src/gooey/ui/notification/Toast/scripts/ToastType.js
@@ -0,0 +1,9 @@
+/**
+ * Toast type constants for Toast component
+ */
+export default class ToastType {
+    static INFO = "info";
+    static SUCCESS = "success";
+    static WARNING = "warning";
+    static ERROR = "error";
+}
