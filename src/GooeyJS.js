1385429f79856df6f376f9fb71ad3ffdb6dbcccd
NetHaven <NetHaven@gmail.com>
2026-02-20 02:11:16 -0500

    Distribution 2.2.0 Update

--- src/GooeyJS.js
+++ src/GooeyJS.js
@@ -8,7 +8,7 @@ const SCRIPT_PATH = new URL(import.meta.url, document.baseURI);
 const PATH = SCRIPT_PATH.href.substring(0, SCRIPT_PATH.href.lastIndexOf('/'));
 
 export default class GooeyJS {
-    static VERSION = "2.1";
+    static VERSION = "2.2";
     static Logger = Logger;
     static _initialized = false;
     static _instance = null;
